327ed06b2fba19f398ce6aa0be2513b71ca69690
OxiBo <xenos2000@ukr.net>
2019-02-12 16:12:46 -0500

    move function 'removeClas' from 'App.js'

---
+++ src/js/helpersFunc/helpers.js
@@ -0,0 +1,22 @@
+console.log("works")
+
+// helper function to remove class from drum-pads
+    export const removeClass = (elementListened, propertyName, classToRemoveName) => {
+
+        elementListened.forEach(pad => pad.addEventListener('transitionend', function(e) {
+
+            if (e.propertyName !== propertyName) return; // skip if it is not transform
+            this.classList.remove(classToRemoveName)
+        }))
+    }
+
+
+    //   // helper function to remove class from drum-pads
+    // removeClass = () => {
+    //     const drumPadElements = document.querySelectorAll(".drum-pad");
+    //     drumPadElements.forEach(pad => pad.addEventListener('transitionend', function(e) {
+
+    //         if (e.propertyName !== "transform") return; // skip if it is not transform
+    //         this.classList.remove('playingPad')
+    //     }))
+    // }
